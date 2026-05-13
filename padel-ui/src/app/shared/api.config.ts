94f2249787b2798017ffcc27e0da9f6fd43db692
Mayohervas03 <mayohervas03@gmail.com>
2026-05-13 23:01:59 +0200

    Fix: corregir ruta de importacion de environment

--- padel-ui/src/app/shared/api.config.ts
+++ padel-ui/src/app/shared/api.config.ts
@@ -1,5 +1,5 @@
 import { InjectionToken } from '@angular/core';
-import { environment } from '../environments/environment';
+import { environment } from '../../environments/environment';
 
 export const API_BASE_URL = new InjectionToken<string>('api.baseUrl', {
   factory: () => environment.apiUrl
